6a7be6e5e5b5a3614f852bb2e82f79ccac2d6d56
Zandri2k <sandrasiphokaziii@gmail.com>
2023-10-03 11:34:35 +0200

    meny

--- Program.cs
+++ Program.cs
@@ -2,6 +2,36 @@
 {
     private static void Main(string[] args)
     {
+        bool isRunning = true;
+        while(isRunning)
+        {
+            Console.WriteLine("[1]: Rapportering av uttryckning");  
+            Console.WriteLine("[2]: Rapporter");
+            Console.WriteLine("[3]: Avsluta");
+            Console.Write("Ditt val: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+                
+                default:
+                    Console.WriteLine("");
+                    break;       
+            }
+            
+
+    
+
+
+        }
         
     }
 }
