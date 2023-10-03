39738187ebee0a09c351ae7c2456f1911ff0a9fa
Sandra Andersson <sandy.andersson.6@gmail.com>
2023-10-03 11:00:13 +0200

    Clean slate

--- Program.cs
+++ Program.cs
@@ -2,16 +2,6 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
-        Console.WriteLine("Hej Sandra");
-
-        Console.WriteLine("Nytt test");
-
-        Console.WriteLine("hej igen");
-
-        Console.WriteLine("nu borde det funka");
-
-        System.Console.WriteLine("Vi testar igen, hej hoj");
+        
     }
 }
