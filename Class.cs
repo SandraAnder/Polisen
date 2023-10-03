f1041c987ed2dee22b4c2ead3bf32b922d5b743e
Sandra Andersson <sandy.andersson.6@gmail.com>
2023-10-03 11:50:58 +0200

    Klassfix

--- Class.cs
+++ Class.cs
@@ -1,36 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-internal class Rapportklass
+namespace Polisen
 {
-    public static void Rapport()
+    public class Polis
     {
-        public int rapportNr;
-        public int datum;
-        public string? polisStation;
-        public string? beskrivning;
-    
-        Console.WriteLine("Lämna rapport");
+        public static void RegUtryck()
+        {
 
-        Console.WriteLine($"RapportNr: {rapportNr}");
-        int inputRapportNr = int.Parse(Console.ReadLine());
-        Console.WriteLine($"Datum: {datum}");
-        int inputDatum = int.Parse(Console.ReadLine());
-        Console.WriteLine($"Polisstation: {polisStation}");
-        string? inputStation = Console.ReadLine();
-        Console.WriteLine($"Beskrivning: {beskrivning}");
-        string? inputBeskrivning = Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void Rapport()
+        {
+            public int rapportNr;
+            public int datum;
+            public string? polisStation;
+            public string? beskrivning;
+        
+            Console.WriteLine("Lämna rapport");
+
+            Console.WriteLine($"RapportNr: {rapportNr}");
+            int inputRapportNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Datum: {datum}");
+            int inputDatum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Polisstation: {polisStation}");
+            string? inputStation = Console.ReadLine();
+
+            Console.WriteLine($"Beskrivning: {beskrivning}");
+            string? inputBeskrivning = Console.ReadLine();
+        }
     }
-
 }
