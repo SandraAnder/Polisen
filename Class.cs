31b793143d899193711ad6528183ddfbaaf3bea4
Sandra Andersson <sandy.andersson.6@gmail.com>
2023-10-03 11:39:04 +0200

    Rapportklass

--- Class.cs
+++ Class.cs
@@ -12,3 +12,25 @@
 
 
 
+internal class Rapportklass
+{
+    public static void Rapport()
+    {
+        public int rapportNr;
+        public int datum;
+        public string? polisStation;
+        public string? beskrivning;
+    
+        Console.WriteLine("Lämna rapport");
+
+        Console.WriteLine($"RapportNr: {rapportNr}");
+        int inputRapportNr = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Datum: {datum}");
+        int inputDatum = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Polisstation: {polisStation}");
+        string? inputStation = Console.ReadLine();
+        Console.WriteLine($"Beskrivning: {beskrivning}");
+        string? inputBeskrivning = Console.ReadLine();
+    }
+
+}
