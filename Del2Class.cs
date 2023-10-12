7f66d5577fba0d77b02a0572181c08c413ec3ea2
Sandra Andersson <sandy.andersson.6@gmail.com>
2023-10-12 10:14:23 +0200

    Färdigt program

--- Del2Class.cs
+++ Del2Class.cs
@@ -11,12 +11,11 @@ public class Police
     public string? Name { get; set; }
     public string? ServiceNr { get; set; }
 
-    public Police(string name, string serviceNr)
+    public Police(string? name, string? serviceNr)
     {
         Name = name;
         ServiceNr = serviceNr;
     }
-   
 }
 
 
@@ -28,111 +27,44 @@ public class Dispatch
     }
 
     public string RegUtrInput { get; }
-
-    public static void RegUtryck(List<Dispatch> ru, string name)
-    {
-
-
-        //flytta över detta till dispatch
-       
-    }
 }  
 
-class Time
+public class Time
 {
     public DateTime DateTimeValue { get; set; }
 
-    public Time(string input)
+    public Time(string? input)
     {
         // Försök att konvertera användarens inmatning till ett DateTime-objekt
         if (DateTime.TryParse(input, out DateTime result))
         {
             DateTimeValue = result;
         }
-        else
-        {
-            Console.WriteLine("Ogiltigt datum och tid format.");
-        }
-    }
-
-    public void DisplayTime()
-    {
-        Console.WriteLine($"Du har angett datum och tid: {DateTimeValue.ToString("yyyy-MM-dd HH:mm:ss")}");
-    }
-}
-
-
-public class Timee
-{
-    public DateTime DateTimeValue { get; set; }
-
-    public Timee(DateTime dateTime)
-    {
-        DateTimeValue = dateTime;
     }
 }
 
 public class Rapporter
 {
-    public int RapportNr { get; set; }
-    public Timee Datum { get; set; }
-    public string PolisStation { get; set; }
-    public string Beskrivning { get; set; }
+    public string? RapportOutput { get; set; }
 
-    public Rapporter(int rapportNr, Timee datum, string polisStation, string beskrivning)
+    public DateTime Datum { get; set; }
+
+    public Rapporter(string rapportOutput, DateTime datum)
+    {
+        RapportOutput = rapportOutput;
+        Datum = datum;
+    }
+}
+public class RapportInfo
+{
+     public int RapportNr { get; set; }
+     public string? PolisStation { get; set; }
+    public string? Beskrivning { get; set; }
+
+    public RapportInfo(int rapportNr, string polisStation, string beskrivning)
     {
         RapportNr = rapportNr;
-        Datum = datum;
         PolisStation = polisStation;
         Beskrivning = beskrivning;
-    }
-    //public static void rapportis()
-}
-
-public class Info
-{
-    public Info(string showInfo)
-    {
-        ShowInfo = showInfo;
-    }
-
-    public string ShowInfo { get; }
-
-    public static void ShowInformation(List<Dispatch> ru, List<Rapporter> ra, List<Police> rp)
-    {
-        Console.WriteLine("Välj vad du vill visa:");
-        Console.WriteLine("1. Utryckningar");
-        Console.WriteLine("2. Rapporter");
-        Console.WriteLine("3. Personal");
-
-        int val = Convert.ToInt32(Console.ReadLine());
-
-        switch (val)
-        {
-            case 1:
-                Console.WriteLine("Utryckningar:");
-                foreach (var rut in ru)
-                {
-                    Console.WriteLine(rut.RegUtrInput);
-                }
-                break;
-            case 2:
-                Console.WriteLine("Rapporter:");
-                foreach (var rapport in ra)
-                {
-                    Console.WriteLine(rapport);
-                }
-                break;
-            case 3:
-                Console.WriteLine("Personal:");
-                foreach (var personal in rp)
-                {
-                    Console.WriteLine($"Namn: {personal.Name}, TjänstNr: {personal.ServiceNr}");
-                }
-                break;
-            default:
-                Console.WriteLine("Ogiltigt val. Försök igen.");
-                break;
-        }
     }
 }
--- Del2Program.cs
+++ Del2Program.cs
@@ -39,21 +39,30 @@ internal class Program
                     Console.WriteLine("====================");
 
                     Console.WriteLine("TID");
-                    double timee = double.Parse(Console.ReadLine());
+                    string? inputTime = Console.ReadLine();
                     Console.WriteLine("====================");
+                    if (DateTime.TryParse(inputTime, out DateTime datum))
+                    {
 
-                    Console.WriteLine("TYP");
-                    string? type = (Console.ReadLine());
-                    Console.WriteLine("====================");
+                        Console.WriteLine("TYP");
+                        string? type = Console.ReadLine();
+                        Console.WriteLine("====================");
 
-                    Console.WriteLine("Polis på plats"); 
-                    Console.Write("Namn: ");
-                    
-                    string? nmPolice = Console.ReadLine();
-                    
-                    string regUtrInput = $"Plats: {location} Tid: {timee}  Typ: {type}  Polis på plats: {nmPolice}";
-                    Dispatch rut = new Dispatch(regUtrInput);
-                    ru.Add(rut);
+                        Console.WriteLine("Polis på plats"); 
+                        Console.Write("Namn: ");
+                        
+                        string? nmPolice = Console.ReadLine();
+                        
+                        string regUtrInput = $"Plats: {location} Tid: {datum}  Typ: {type}  Polis på plats: {nmPolice}";
+                        Dispatch rut = new Dispatch(regUtrInput);
+                        ru.Add(rut);
+
+                        Console.WriteLine($"Din rapport är registrerad");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ogiltigt datumformat.");
+                    }
                     break;
 
                 case 2:
@@ -66,7 +75,7 @@ internal class Program
                     Console.Write("Datum (ÅÅÅÅ-MM-DD HH:mm:ss): ");
                     string? inputDatum = Console.ReadLine();
                     Console.WriteLine("====================");
-                    if (DateTime.TryParse(inputDatum, out DateTime datum))
+                    if (DateTime.TryParse(inputDatum, out DateTime datum1))
                     {
                         Console.Write("Polisstation: ");
                         string? inputStation = Console.ReadLine();
@@ -76,18 +85,17 @@ internal class Program
                         string? inputBeskrivning = Console.ReadLine();
                         Console.WriteLine("====================");
 
-                        string rapportOutput = $"Rapportnummer: {inputRapportNr} Tid: {datum}  Beskrivnign: {inputBeskrivning}";
-                        Rapporter rapport = new Rapporter(rapportOutput, datum); //datum går inte in, hjälp
+                        string rapportOutput = $"Rapportnummer: {inputRapportNr} Tid: {datum1} Polisstation: {inputStation} Beskrivning: {inputBeskrivning}";
+                        Rapporter rapport = new Rapporter(rapportOutput, datum1);
                         ra.Add(rapport);
                        
-                        Console.WriteLine($"Rapporten har lagts till: RapportNr: {rapport.RapportNr}, Datum: {rapport.Datum.DateTimeValue}, Polisstation: {rapport.PolisStation}, Beskrivning: {rapport.Beskrivning}");
+                        Console.WriteLine($"Rapporten har lagts till: RapportNr: {inputRapportNr}, Datum: {datum1.ToString("yyyy-MM-dd HH:mm:ss")}, Polisstation: {inputStation}, Beskrivning: {inputBeskrivning}");
                     }
                     else
                     {
                         Console.WriteLine("Ogiltigt datumformat.");
                         
                     }
-                   
                     break;
 
                 case 3:
@@ -101,14 +109,46 @@ internal class Program
                     Console.WriteLine("Alla namn i listan:");
                     for (int i = 0; i < rp.Count; i++)
                     {
-                    
                         Console.WriteLine($"Namn: {rp[i].Name} tjänstenummer: {rp[i].ServiceNr}");
                     }
                     Console.WriteLine();
                     
                     break;
                 case 4:
-                    Info.ShowInformation(ru, ra, rp);
+                    Console.WriteLine("Välj vad du vill visa:");
+                    Console.WriteLine("1. Utryckningar");
+                    Console.WriteLine("2. Rapporter");
+                    Console.WriteLine("3. Personal");
+
+                    int val = Convert.ToInt32(Console.ReadLine());
+
+                    switch (val)
+                    {
+                        case 1:
+                            Console.WriteLine("Utryckningar:");
+                            foreach (var rut in ru)
+                            {
+                                Console.WriteLine(rut.RegUtrInput);
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Rapporter:");
+                            foreach (var rapport in ra)
+                            {
+                                Console.WriteLine(rapport.RapportOutput);
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Personal:");
+                            foreach (var p in rp)
+                            {
+                                Console.WriteLine($"Namn: {p.Name}, TjänstNr: {p.ServiceNr}");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Ogiltigt val. Försök igen.");
+                            break;
+                    }
                     break;
 
                 case 5:
